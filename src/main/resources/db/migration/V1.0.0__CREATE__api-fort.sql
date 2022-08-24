3cb3a67d724df929d3fa4f9c82320d0ae330ad16
iabughosh <ibrahim.a@sitech.me>
2022-08-24 13:02:50 +0300

    Fixing db migration scripts.

--- src/main/resources/db/migration/V1.0.0__CREATE__api-fort.sql
+++ src/main/resources/db/migration/V1.0.0__CREATE__api-fort.sql
@@ -1,6 +1,15 @@
 
+    create table client_profile (
+        id bigint AUTO_INCREMENT not null,
+        api_key varchar(36),
+        auth_claim_key varchar(60) not null,
+        jwt_public_certificate varchar(2500),
+        realm varchar(50),
+        uuid varchar(36) not null,
+        primary key (id)
+    ) engine=InnoDB;
     create table client_endpoints (
-       id bigint not null,
+        id bigint AUTO_INCREMENT not null,
         is_activate bit,
         auth_claim_value varchar(255) not null,
         clients_profile_uuid varchar(255) not null,
@@ -12,16 +21,6 @@
         is_terminate bit,
         uuid varchar(36) not null,
         version_number integer,
-        primary key (id)
-    ) engine=InnoDB;
-
-    create table client_profile (
-       id bigint not null,
-        api_key varchar(36),
-        auth_claim_key varchar(60) not null,
-        jwt_public_certificate varchar(2500),
-        realm varchar(50),
-        uuid varchar(36) not null,
         primary key (id)
     ) engine=InnoDB;
 
--- src/main/resources/db/migration/V1.0.1__INSERT__api-fort.sql
+++ src/main/resources/db/migration/V1.0.1__INSERT__api-fort.sql
@@ -1,20 +1,19 @@
 --
--- Dumping data for table `client_endpoints`
---
-
-LOCK TABLES `client_endpoints` WRITE;
-
-INSERT INTO `client_endpoints` VALUES (3,_binary '\0','uma_authorization','24ac7b3b-9a2a-4f96-8887-87a11dc7f878','/cards','/cards','GET',_binary '','omni-ops-customer-dashboard:8000',_binary '\0','fa5f27c1-64dd-4a3b-b8a0-fcd70c815270',1),(4,_binary '\0','uma_authorization','24ac7b3b-9a2a-4f96-8887-87a11dc7f878','/cards/stats','/cards/stats','GET',_binary '','omni-ops-customer-dashboard:8000',_binary '\0','a0ca1311-9afa-4727-8bef-aa121ce79fbc',1),(5,_binary '\0','uma_authorization','24ac7b3b-9a2a-4f96-8887-87a11dc7f878','/cards/analytics','/cards/analytics','GET',_binary '','omni-ops-customer-dashboard:8000',_binary '\0','78ffbb17-868d-4a82-9a00-62496754ae64',1),(6,_binary '\0','uma_authorization','24ac7b3b-9a2a-4f96-8887-87a11dc7f878','/cards/lookups','/cards/lookups','GET',_binary '','omni-ops-customer-dashboard:8000',_binary '\0','5edc719b-42b6-40cc-8587-b3fd42633fdb',1);
-
-UNLOCK TABLES;
-
-
---
 -- Dumping data for table `client_profile`
 --
 
-LOCK TABLES `client_profile` WRITE;
+INSERT INTO `client_profile`(api_key, auth_claim_key, jwt_public_certificate, realm, uuid)
+VALUES ('AZ87-6563-XUJH-00001','realm_access','MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAhEyH6WOL5VHr2hKsYKdD1yEMBw8kovV5kr/yth+P2OanuQ03p4TwFMYxYTdHrLbPOlsJPf5YxIzd7PWCgWYNsTWunPOIE/f5wOkbdoPkbds/buwMYPAeEpTEqAn515X1aFUj2NR50UeLphawXjHVbOI4fpeByPbhhFMpBVys+07tKGscPmc4BWMlFMDYl08VPg9Y12rVDBod6rD9f4Cg92zzLzu1Bjao/UbRsqtfU2ijtukB0kigyeI+EJTrxDxYCllFiKJcOhOSlzQM/W9G11DmN1LPoOEDCVtKHNcAx/rvI/8TFKTmsnKILEsJVXxfKs0SR4UtqsVKgfiWgTeWrwIDAQAB','sitech','24ac7b3b-9a2a-4f96-8887-87a11dc7f878');
 
-INSERT INTO `client_profile` VALUES (2,'AZ87-6563-XUJH-00001','realm_access','MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAhEyH6WOL5VHr2hKsYKdD1yEMBw8kovV5kr/yth+P2OanuQ03p4TwFMYxYTdHrLbPOlsJPf5YxIzd7PWCgWYNsTWunPOIE/f5wOkbdoPkbds/buwMYPAeEpTEqAn515X1aFUj2NR50UeLphawXjHVbOI4fpeByPbhhFMpBVys+07tKGscPmc4BWMlFMDYl08VPg9Y12rVDBod6rD9f4Cg92zzLzu1Bjao/UbRsqtfU2ijtukB0kigyeI+EJTrxDxYCllFiKJcOhOSlzQM/W9G11DmN1LPoOEDCVtKHNcAx/rvI/8TFKTmsnKILEsJVXxfKs0SR4UtqsVKgfiWgTeWrwIDAQAB','sitech','24ac7b3b-9a2a-4f96-8887-87a11dc7f878');
+--
+-- Dumping data for table `client_endpoints`
+--
 
-UNLOCK TABLES;
+INSERT INTO client_endpoints(is_activate, clients_profile_uuid, auth_claim_value, endpoint_path, endpoint_regex, method_type, offline_authentication, service_name, is_terminate, uuid, version_number)
+VALUES (0, 'AZ87-6563-XUJH-00001','uma_authorization','/cards','/cards','GET',1,'omni-ops-customer-dashboard:8000',0,'fa5f27c1-64dd-4a3b-b8a0-fcd70c815270',1);
+INSERT INTO client_endpoints(is_activate, clients_profile_uuid, auth_claim_value, endpoint_path, endpoint_regex, method_type, offline_authentication, service_name, is_terminate, uuid, version_number)
+VALUES (0, 'AZ87-6563-XUJH-00001','uma_authorization','/cards/stats','/cards/stats','GET',1,'omni-ops-customer-dashboard:8000',0,'a0ca1311-9afa-4727-8bef-aa121ce79fbc',1);
+INSERT INTO client_endpoints(is_activate, clients_profile_uuid, auth_claim_value, endpoint_path, endpoint_regex, method_type, offline_authentication, service_name, is_terminate, uuid, version_number)
+VALUES (0, 'AZ87-6563-XUJH-00001','uma_authorization','/cards/analytics','/cards/analytics','GET',1,'omni-ops-customer-dashboard:8000',0,'78ffbb17-868d-4a82-9a00-62496754ae64',1);
+INSERT INTO client_endpoints(is_activate, clients_profile_uuid, auth_claim_value, endpoint_path, endpoint_regex, method_type, offline_authentication, service_name, is_terminate, uuid, version_number)
+VALUES (0, 'AZ87-6563-XUJH-00001','uma_authorization','/cards/lookups','/cards/lookups','GET',1,'omni-ops-customer-dashboard:8000',0,'5edc719b-42b6-40cc-8587-b3fd42633fdb',1);
