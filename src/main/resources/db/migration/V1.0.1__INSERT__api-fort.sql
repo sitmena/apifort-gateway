8b1cb3031c2c01cf6de886d93cd20fa1ae68c031
iabughosh <ibrahim.a@sitech.me>
2022-09-06 12:11:01 +0300

    Rollback script file and fix service name.

--- src/main/resources/db/migration/V1.0.1__INSERT__api-fort.sql
+++ src/main/resources/db/migration/V1.0.1__INSERT__api-fort.sql
@@ -10,14 +10,10 @@
 --
 
 INSERT INTO client_endpoints(uuid,clients_profile_uuid,service_name,context_path,endpoint_path,endpoint_regex,method_type,auth_claim_value,offline_authentication,is_public_service,version_number,is_activate,is_terminate)
-VALUES ('5161d0d3-cbd1-41cf-9080-1c55a81cac7c','af3ff92f-427b-43e8-a4ac-530a34c4620a','omni-ops-customer-dashboard:8000','omni-ops-customer-dashboard','/cards','^/apis/cards$','GET','uma_authorization',0,1,1,0,1);
-
+VALUES ('5161d0d3-cbd1-41cf-9080-1c55a81cac7c','af3ff92f-427b-43e8-a4ac-530a34c4620a','omni-ops.omni-ops-customer-dashboard-service.svc.cluster.local:8000','omni-ops-customer-dashboard','/cards','^/apis/omni-ops-customer-dashboard/cards$','GET','uma_authorization',0,1,1,0,1);
 INSERT INTO client_endpoints(uuid,clients_profile_uuid,service_name,context_path,endpoint_path,endpoint_regex,method_type,auth_claim_value,offline_authentication,is_public_service,version_number,is_activate,is_terminate)
-VALUES ('66f9bba9-ad2d-48af-aaaf-70cc2fe2d76c','af3ff92f-427b-43e8-a4ac-530a34c4620a','omni-ops-customer-dashboard:8000','omni-ops-customer-dashboard','/cards/stats','^/apis/cards/stats$','GET','uma_authorization',0,1,1,0,1);
-
+VALUES ('96d2a845-8008-4c17-949a-08e5bbbe65a8','af3ff92f-427b-43e8-a4ac-530a34c4620a','omni-ops.omni-ops-customer-dashboard-service.svc.cluster.local:8000','omni-ops-customer-dashboard','/cards/lookups','^/apis/omni-ops-customer-dashboard/cards/lookups$','GET','uma_authorization',0,1,1,0,1);
 INSERT INTO client_endpoints(uuid,clients_profile_uuid,service_name,context_path,endpoint_path,endpoint_regex,method_type,auth_claim_value,offline_authentication,is_public_service,version_number,is_activate,is_terminate)
-VALUES ('59d0b79d-bd75-4a51-8ff0-ad12a8eef3df','af3ff92f-427b-43e8-a4ac-530a34c4620a','omni-ops-customer-dashboard:8000','omni-ops-customer-dashboard','/cards/analytics','^/apis/cards/analytics$','GET','uma_authorization',0,1,1,0,1);
-
+VALUES ('59d0b79d-bd75-4a51-8ff0-ad12a8eef3df','af3ff92f-427b-43e8-a4ac-530a34c4620a','omni-ops.omni-ops-customer-dashboard-service.svc.cluster.local:8000','omni-ops-customer-dashboard','/cards/analytics','^/apis/omni-ops-customer-dashboard/cards/analytics$','GET','uma_authorization',0,1,1,0,1);
 INSERT INTO client_endpoints(uuid,clients_profile_uuid,service_name,context_path,endpoint_path,endpoint_regex,method_type,auth_claim_value,offline_authentication,is_public_service,version_number,is_activate,is_terminate)
-VALUES ('96d2a845-8008-4c17-949a-08e5bbbe65a8','af3ff92f-427b-43e8-a4ac-530a34c4620a','omni-ops-customer-dashboard:8000','omni-ops-customer-dashboard','/cards/lookups','^/apis/cards/lookups$','GET','uma_authorization',0,1,1,0,1);
-
+VALUES ('66f9bba9-ad2d-48af-aaaf-70cc2fe2d76c','af3ff92f-427b-43e8-a4ac-530a34c4620a','omni-ops.omni-ops-customer-dashboard-service.svc.cluster.local:8000','omni-ops-customer-dashboard','/cards/stats','^/apis/omni-ops-customer-dashboard/cards/stats$','GET','uma_authorization',0,1,1,0,1);
