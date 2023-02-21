3c139b1d500c1b3951049e66e2a729c8b80c002e
Firas Abbasi <firas@sitech.me>
2023-02-21 15:22:43 +0300

    #132 404 Not Found for unregistered APIs

--- src/main/java/me/sitech/apifort/processor/GatewayExceptionHandlerProcessor.java
+++ src/main/java/me/sitech/apifort/processor/GatewayExceptionHandlerProcessor.java
@@ -3,6 +3,10 @@ package me.sitech.apifort.processor;
 import io.opentelemetry.api.trace.Span;
 import lombok.extern.slf4j.Slf4j;
 import me.sitech.apifort.constant.ApiFort;
+import me.sitech.apifort.constant.ApiFortStatusCode;
+import me.sitech.apifort.domain.response.common.ErrorRes;
+import me.sitech.apifort.exceptions.APIFortGeneralException;
+import me.sitech.apifort.exceptions.ApiFortInvalidEndpoint;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
@@ -17,8 +21,12 @@ public class GatewayExceptionHandlerProcessor implements Processor {
     @Override
     public void process(Exchange exchange) throws Exception {
         final Throwable ex = exchange.getProperty(Exchange.EXCEPTION_CAUGHT, Throwable.class);
-
         String traceId = Span.current().getSpanContext().getTraceId();
+
+        if(ex instanceof ApiFortInvalidEndpoint || ex instanceof APIFortGeneralException){
+            exchange.getIn().setHeader(Exchange.HTTP_RESPONSE_CODE, ApiFortStatusCode.BAD_REQUEST);
+            exchange.getIn().setBody(new ErrorRes(traceId,ex.getMessage()));
+        }
         exchange.getIn().setHeader(ApiFort.APIFORT_TRACE_ID, traceId);
         exchange.getIn().removeHeader(APIFORT_DOWNSTREAM_SERVICE_HEADER);
 
