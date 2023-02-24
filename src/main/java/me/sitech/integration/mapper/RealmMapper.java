21326eac158aa5a34cbbdc379284656a5574e25a
ajweh777 <mohammad.a@sitech.me>
2023-02-25 01:57:34 +0300

    remove unused class

--- src/main/java/me/sitech/integration/mapper/RealmMapper.java
+++ src/main/java/me/sitech/integration/mapper/RealmMapper.java
@@ -1,11 +1,11 @@
 package me.sitech.integration.mapper;
 
 import me.sitech.integration.domain.module.Dto;
-import me.sitech.integration.domain.response.ProfileUserResponse;
 import me.sitech.integration.domain.response.realm.RealmClientResponse;
 import me.sitech.integration.domain.response.realm.RealmGroupResponse;
 import me.sitech.integration.domain.response.realm.RealmResponse;
 import me.sitech.integration.domain.response.realm.RealmRoleResponse;
+import me.sitech.integration.domain.response.user.UserResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.factory.Mappers;
@@ -19,7 +19,7 @@ public interface RealmMapper {
     RealmResponse toDto(Dto.RealmDto realmDto);
     List<RealmResponse> toDtoList(List<Dto.RealmDto> realmDto);
     List<RealmGroupResponse> toGroupDtoList(List<Dto.GroupDto> groupDto);
-    List<ProfileUserResponse> toUserDtoList(List<Dto.UserDto> userDto);
+    List<UserResponse> toUserDtoList(List<Dto.UserDto> userDto);
     List<RealmClientResponse> toClientDto(List<Dto.ClientDto> clientDto);
     List<RealmRoleResponse> toRoleDto(List<Dto.RoleDto> roleDto);
 }
